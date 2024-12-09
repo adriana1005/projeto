e6eab69ac8fffe11a49f402d4ecd204fe252a87e
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 03:03:52 +0000

    Documento para analisar o que falta fazer, o que já foi feita e ver as horas que cada um está a trabalhar.

---
+++ A_FAZER.py
@@ -0,0 +1,43 @@
+'''FEITOS:
+extrair a informação do documento (Adriana)
+separar informação por tópicos: (Adriana)
+Contar o número de tweets por sentimento(Adriana)
+
+'''
+
+'''
+OBRIGATORIOS:
+-Calcular a percentagem de cada tipo de sentimento para todas as
+companhias aéreas. (em processo)
+
+-Identificar a companhia aérea com o maior número de tweets positivos.
+
+-Analisar o número médio de retweets por tipo de sentimento
+
+-Listar todas as companhias aéreas mencionadas no dataset
+
+-Identificar a companhia com mais tweets negativos (em processo)
+
+-Calcular o número total de tweets por companhia.(em processo)
+ 
+- Filtrar os tweets de uma companhia específica e mostrar seus detalhes.
+
+-Identificar o dia com maior número de tweets.
+
+- Contar quantos tweets foram feitos num determinado mês ou ano.
+
+-Fazer o READ.ME
+
+-Fazer o relatório no jupyter
+ 
+ 
+IDEIAS PARA MELHORAR A NOTA:
+Criar um menu para iterativo a perguntar por cada coisa, com cores e bonitinho
+
+
+TOTAL DE HORAS:
+Adriana: +/- 15 horas  
+Carlota:
+Simão:
+
+'''
