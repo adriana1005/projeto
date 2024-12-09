9b04911e9ff0a2ee8d90b8e2f7d5234d0ab31e80
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 05:23:48 +0000

    mudanças relativas à mudança dos pops

--- funcoes/sentimento_airline_lista.py
+++ funcoes/sentimento_airline_lista.py
@@ -1,12 +1,8 @@
-from funcoes.airline_sentiment import airline_sent_pop
-from funcoes.airline import airline_pop
 
-#airline = airline_pop()
-#sentiment = airline_sent_pop()
+from funcoes.separarinformacao import *
 
 
-
-
+separar_info() #Chamar a função das listas
 
 
 def lista_zip():
@@ -14,14 +10,15 @@ def lista_zip():
     os sentimentos dos comentários pela airline para fazer as percentagens'''
     #print(len(airline_pop()))
     #print(len(sentiment))
-    lista_res = list(zip(airline_pop(), airline_sent_pop()))
+    lista_res = list(zip(airline, airline_sentiment))
 
-
-    print(lista_res)
 
     return lista_res
+
+
 
 '''FALTA: criar função para analisar e fazer as percentagens 
 dos sentimentos em função à airline(PODE SER DENTRO DA OUTRA)'''
 
 lista_zip()
+
