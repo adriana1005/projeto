910e499af15ad95524e6ba11d738cd95a9dd804b
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 05:24:37 +0000

    simplificação visual e comentários

--- funcoes/ler_documento.py
+++ funcoes/ler_documento.py
@@ -1,4 +1,5 @@
-import os, csv
+import os
+import csv
 
 
 path = "C:\\CodePython\\Mymodulles\\archive (1)\\"
@@ -11,7 +12,9 @@ lista = []
 def ler_documento():
     '''Definir uma lista com todos os dados retirados do documento'''
     with open(full_path, 'r', encoding='utf-8', errors='ignore') as file:
-        conteudo = csv.reader(file, delimiter=',', quotechar='"', skipinitialspace=True)  # Trata das vírgulas internas
+        conteudo = csv.reader(file, delimiter=',', quotechar='"', skipinitialspace=True)
+        '''Trata das vírgulas internas , quotechar serve para delimitar cada célula
+         skipinitialspace serve para ignorar espaços iniciais'''
 
         for linha in conteudo:
             lista.append(linha)
