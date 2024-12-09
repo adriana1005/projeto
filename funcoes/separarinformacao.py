feefd1619b2e3da3a1ac2a90b7fbaf38514c616f
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 03:07:16 +0000

    criação de comentários

--- funcoes/separarinformacao.py
+++ funcoes/separarinformacao.py
@@ -3,7 +3,7 @@ from funcoes.ler_documento import *
 
 tweet_id = []  #0
 airline_sentiment = []  #1
-airline_sentiment_confidence = [] #2
+sentiment_c = [] #2
 negativereason = [] #3
 negativereason_confidence = [] #4
 airline = [] #5
@@ -21,16 +21,16 @@ user_timezone = [] #14
 def separar_info():
 
     for linha in lista:
+
         if len(linha) != 15:
             print(lista.index(linha))
             print(len(linha))
             print(linha)
             continue
-        #print(linha)
 
         tweet_id.append(linha[0])
         airline_sentiment.append(linha[1])
-        airline_sentiment_confidence.append(linha[2])
+        sentiment_c.append(linha[2])
         negativereason.append(linha[3])
         negativereason_confidence.append(linha[4])
         airline.append(linha[5])
@@ -44,8 +44,5 @@ def separar_info():
         tweet_location.append(linha[13])
         user_timezone.append(linha[14])
 
-    return [tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, retweet_count, text,
-            tweet_coord, tweet_created, tweet_location, user_timezone]
-
-
-print(separar_info()[2])
+    return [tweet_id, airline_sentiment, sentiment_c, negativereason, negativereason_confidence, airline,
+            airline_sentiment_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone]
