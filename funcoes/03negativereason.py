a27455a53b24d6fb9d1c56dc29731dd15f4252a5
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 04:04:47 +0000

    Remoção de arquivos

--- funcoes/03negativereason.py
+++
@@ -1,7 +0,0 @@
-from funcoes.separarinformacao import *
-import matplotlib.pyplot as plt
-
-def contagem
-def grafico_negative_reason():
-    valores=
-    categorias=
--- funcoes/05airline.py
+++
@@ -1,1 +0,0 @@
-from funcoes.separarinformacao import *
