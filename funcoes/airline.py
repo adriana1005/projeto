27eef899aa9b7a12fed8f52718a43c9e5722f558
adriana1005 <adrianaabreu1005@gmail.com>
2024-12-09 02:47:14 +0000

    criação da função para retirar o primeiro elemento da lista e criar comentários

---
+++ funcoes/airline.py
@@ -0,0 +1,10 @@
+from funcoes.separarinformacao import *
+'''Trabalhar a lista de dados dos nomes das airlines'''
+
+separar_info()
+
+
+def airline_pop():
+    '''Retirar o primeiro elemento da lista dos nomes das airlines'''
+    airline.pop(0)
+    return airline
